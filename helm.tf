e91d25a5d32ff3c096c62d65ba9819022bf6e463
Alon Gritsovsky <alongritsovski@gmail.com>
2025-08-31 11:36:03 +0300

    add new structure

--- helm.tf
+++ helm.tf
@@ -1,22 +1,11 @@
-resource "helm_release" "mongodb" {
-  name      = "mongodb"
-  chart     = "../Portfolio-Cluster/charts/mongodb-chart"
+resource "helm_release" "portfolio_umbrella" {
+  name      = "portfolio-umbrella"
+  chart     = "../Portfolio-Cluster/charts/portfolio-umbrella"
   namespace = "default"
 
   values = [
-    file("../Portfolio-Cluster/charts/mongodb-chart/values.yaml")
+    file("../Portfolio-Cluster/charts/portfolio-umbrella/values.yaml")
   ]
+
   depends_on = [module.eks]
-}
-
-resource "helm_release" "flask_app" {
-  name      = "flask-app"
-  chart     = "../Portfolio-Cluster/charts/flask-app"
-  namespace = "default"
-
-  values = [
-    file("../Portfolio-Cluster/charts/flask-app/values.yaml")
-  ]
-
-  depends_on = [helm_release.mongodb]
 }
